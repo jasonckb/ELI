f846afaad11a8b35cd008ad9284c1be1d2e04dc2
jasonckb <122160477+jasonckb@users.noreply.github.com>
2024-08-20 07:40:56 +0800

    Add files via upload

--- streamlit_ELI.py
+++ streamlit_ELI.py
@@ -242,7 +242,7 @@ def get_financial_data(ticker):
     # Balance sheet data
     balance_sheet = stock.balance_sheet
     financials['total_debt'] = balance_sheet.loc['Total Debt'].iloc[0] if 'Total Debt' in balance_sheet.index else 0
-    financials['cash_and_cash_equivalents'] = balance_sheet.loc['Cash Cash Equivalents and Short Term Investments'].iloc[0] if 'Cash, Cash Equivalents & Short Term Investments' in balance_sheet.index else 0
+    financials['cash_and_cash_equivalents'] = balance_sheet.loc['Cash Cash Equivalents and Short Term Investments'].iloc[0] if 'Cash Cash Equivalents and Short Term Investments' in balance_sheet.index else 0
     financials['total_equity'] = balance_sheet.loc['Common Stock Equity'].iloc[0] if 'Common Stock Equity' in balance_sheet.index else 0
     financials['net_debt'] = balance_sheet.loc['Net Debt'].iloc[0] if 'Net Debt' in balance_sheet.index else 0
     
